30a966c0c38eda52c305d14745a2ed412a7765d9
enjung <enjung0915@gmail.com>
2025-02-16 18:02:50 +0900

    feat : 그룹 목록 조회

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -10,12 +10,12 @@ const createGroup = async (req, res) => {
     if (!name || !password) {
       return res.status(400).json({ message: "잘못된 요청입니다" });
     }
-    console.log("🔹 원본 비밀번호:", password);
+    //console.log("원본 비밀번호:", password);
 
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
     
-    console.log("🔐 해싱된 비밀번호:", hashedPassword);
+    //console.log("해싱된 비밀번호:", hashedPassword);
 
     const newGroup = await Group.createGroup({
       name,
@@ -27,7 +27,6 @@ const createGroup = async (req, res) => {
       postCount: 0,
       badgeCount: 0,
     });
-    console.log("✅ Prisma에 저장될 데이터:", newGroup);
 
     res.status(201).json({
       id: newGroup.groupId,
@@ -46,4 +45,52 @@ const createGroup = async (req, res) => {
   }
 };
 
-export default createGroup;
+//그룹 조회
+const getGroups = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 10;
+    const sortBy = req.query.sortBy || "latest";
+    const keyword = req.query.keyword || "";
+    const isPublic = req.query.isPublic !== undefined ? req.query.isPublic === "true" : null;
+
+    const { totalItemCount, groups } = await Group.getGroupsFromDB({
+      page,
+      pageSize,
+      sortBy,
+      keyword,
+      isPublic,
+    });
+
+    const totalPages = Math.ceil(totalItemCount / pageSize);
+    if (totalItemCount === 0) {
+      return res.status(200).json({
+        currentPage: page,
+        totalPages: 0,
+        totalItemCount: 0,
+        data: [],
+      });
+    }
+
+    res.status(200).json({
+      currentPage: page,
+      totalPages: totalPages,
+      totalItemCount: totalItemCount,
+      data: groups.map((group) => ({
+        id: group.groupId,
+        name: group.name,
+        introduction: group.introduction,
+        imageUrl: group.imageUrl,
+        isPublic: group.isPublic,
+        likeCount: group.likeCount,
+        postCount: group.postCount,
+        badgeCount: group.badgeCount,
+        createdAt: group.createdAt,
+      })),
+    });
+  } catch (error) {
+    console.error("Error fetching groups:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export { createGroup, getGroups };
