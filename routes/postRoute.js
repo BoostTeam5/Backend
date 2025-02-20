92314f6632a437180b3793dd0e9d24fdcdaf318e
laura-jung <jungya@ewhain.net>
2025-02-20 23:02:40 +0900

    fix : postDetail API

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -8,13 +8,16 @@ const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 // 게시글 상세 정보 조회
-router.get("/api/:postId", postController.getPostById);
+router.get("/api/posts/:postId", postController.getPostById);
 // 게시글 조회 권한 확인
-router.post("/api/:postId/verify-password", postController.verifyPostPassword);
+router.post(
+  "/api/posts/:postId/verify-password",
+  postController.verifyPostPassword
+);
 // 게시글 공감하기
-router.post("/api/:postId/like", postController.likePost);
+router.post("/api/posts/:postId/like", postController.likePost);
 // 게시글 공개 여부 확인
-router.get("/api/:postId/is-public", postController.isPostPublic);
+router.get("/api/posts/:postId/is-public", postController.isPostPublic);
 //게시글 등록
 router.post(
   "/api/groups/:groupId/posts",
